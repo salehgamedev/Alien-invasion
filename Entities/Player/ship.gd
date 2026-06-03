2a85aa899ae2bedc06c795a2d12cd1450f122856
Saleh Tarrap <salehtarrap@gmail.com>
2026-06-03 22:10:41 +0400

    added asteroids and BG

--- Entities/Player/ship.gd
+++ Entities/Player/ship.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 @export_category("configurations")
 @export var turn_speed: float = 6.0
+@export var ship_deccel: float = 40.0
 @export var throttle_accel: float = 200.0
 @export var idle_deccel: float = 40.0
 @export var max_throttle_speed: float = 100.0
@@ -10,16 +11,16 @@ extends CharacterBody2D
 @export var ship_base: Sprite2D
 @export var thruster_flames: AnimatedSprite2D
 @export var laser: PackedScene
+@export var collision_detector: Area2D
 
-var linear_accel: float = 0:
-	get:
-		if Input.is_action_pressed("accelerate"):
-			return throttle_accel
-		else:
-			return -idle_deccel
-var throttle_speed: float = 0:
+var is_speedboosting: bool = false
+var ship_velocity: Vector2 = Vector2.ZERO
+var throttle_velocity: Vector2:
 	set(value):
-		throttle_speed = clamp(value, 0, max_throttle_speed)
+		throttle_velocity = value.limit_length(max_throttle_speed)
+var throttle_speed: float = 0:
+	get:
+		return throttle_velocity.length()
 var throttle_direction: Vector2:
 	get:
 		return Vector2.from_angle(rotation-PI/2)
@@ -31,7 +32,7 @@ func _ready() -> void:
 	thruster_flames.play("white ship")
 
 func _process(_delta: float) -> void:
-	thruster_flames.visible = linear_accel > 0
+	thruster_flames.visible = Input.is_action_pressed("accelerate")
 	
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
@@ -39,8 +40,15 @@ func _process(_delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	rotation += turn_direction * turn_speed * delta
 	
-	throttle_speed += linear_accel * delta
-	velocity = throttle_speed * throttle_direction
+	if Input.is_action_pressed("accelerate"):
+		throttle_velocity += throttle_accel * throttle_direction * delta
+	else:
+		throttle_velocity -= idle_deccel * throttle_velocity.normalized() * delta
+	
+	apply_speed_booster_boost(delta)
+	ship_velocity -= ship_deccel * delta * ship_velocity.normalized()
+	
+	velocity = throttle_velocity + ship_velocity
 	
 	move_and_slide()
 
@@ -54,3 +62,8 @@ func shoot() -> void:
 	var shot2 = shot.duplicate()
 	get_parent().call_deferred("add_child", shot1)
 	get_parent().call_deferred("add_child", shot2)
+
+func apply_speed_booster_boost(delta: float) -> void:
+	for area in collision_detector.get_overlapping_areas():
+		if area.is_in_group("speed_booster"):
+			ship_velocity += area.speed_boost * delta * Vector2.from_angle(area.rotation-PI/2)
